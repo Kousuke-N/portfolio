860782a88b4a212b8044ae14b91efad8a2362f6b
根津宏輔 <kousuke@ip-192-168-0-10.ap-northeast-1.compute.internal>
2020-03-11 16:23:47 +0900

    worksページ

--- src/app/pages/works/works-list/works-list.component.ts
+++ src/app/pages/works/works-list/works-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Work } from '@/models';
+import { WorkService } from '@/services';
 
 @Component({
   selector: 'app-works-list',
@@ -6,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./works-list.component.scss']
 })
 export class WorksListComponent implements OnInit {
+  works: Work[]
 
-  constructor() { }
+  constructor(private workService: WorkService) {
+    this.works = this.workService.fetchWorks();
+  }
 
   ngOnInit(): void {
   }
--- src/app/pages/works/works-page.module.ts
+++ src/app/pages/works/works-page.module.ts
@@ -3,9 +3,10 @@ import { WorksDetailComponent } from './works-detail/works-detail.component';
 import { WorksListComponent } from './works-list/works-list.component';
 import { WorksPagesRoutingModule } from './works-pages-routing.module';
 import { ComponentsModule } from '@/components/components.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [WorksDetailComponent,WorksListComponent],
-  imports: [WorksPagesRoutingModule, ComponentsModule]
+  imports: [CommonModule, WorksPagesRoutingModule, ComponentsModule]
 })
 export class WorksPagesModule{}
